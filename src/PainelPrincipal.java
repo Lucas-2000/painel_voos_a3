5bd84fdd43a82acd28cb6c814517ace566fdba4b
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-09-27 12:58:23 -0300

    [feat] Layout iniciado

---
+++ src/PainelPrincipal.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class PainelPrincipal extends JFrame {
+    private JPanel MainPanel;
+    private JPanel NovoVooPanel;
+    private JPanel DashboardPanel;
+    private JTextField txtCodigo;
+    private JLabel lblCodigo;
+    private JLabel lblHorario;
+    private JTextField txtHorário;
+    private JButton btnCadastrar;
+    private JLabel lblDestino;
+    private JTextField txtDestino;
+    private JList listVoosConfirmados;
+    private JComboBox cbStatus;
+    private JLabel lblStatus;
+    private JList listVoosAtrasadosCancelados;
+    private JLabel lblVoosConfirmados;
+    private JLabel lblVoosAtrasadosCancelados;
+    private JSeparator separator;
+
+    public PainelPrincipal() {
+        setContentPane(MainPanel);
+        setTitle("Painel Principal");
+        setSize(600, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new PainelPrincipal();
+    }
+}
